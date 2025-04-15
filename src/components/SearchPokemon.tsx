cd95106826126bb7785498b065e2cf1f406da1b7
Lean Junio <leanjunio@outlook.com>
2025-04-15 13:12:39 -0400

    feat: create search component

--- src/components/SearchPokemon.tsx
+++ src/components/SearchPokemon.tsx
@@ -5,13 +5,13 @@ export default function SearchPokemon() {
         Pokemon Name
       </label>
       <div className="input-group">
-        <span className="input-group-text bg-white border-end-0">
+        <span className="input-group-text bg-white border-end-0 ps-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="14"
+            height="14"
             fill="currentColor"
-            className="bi bi-search text-secondary"
+            className="bi bi-search text-secondary opacity-50"
             viewBox="0 0 16 16"
           >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
@@ -19,7 +19,7 @@ export default function SearchPokemon() {
         </span>
         <input
           type="search"
-          className="form-control border-start-0"
+          className="form-control border-start-0 fs-6 px-1 py-2"
           id="pokemon-name"
           placeholder="Search"
         />
