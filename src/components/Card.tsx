3afc799dd95ae8abb6b328e9b7a019edf5297d47
Lean Junio <leanjunio@outlook.com>
2025-04-17 12:26:20 -0400

    refactor: card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+type CardProps = {
+  text: string
+  imageUrl: string
+  altText: string
+}
+
+export default function Card({ text, imageUrl, altText }: CardProps) {
+  return (
+    <div className="card p-2 rounded-1 bg-pokemon-card-border">
+      <div
+        className="bg-pokemon-card ratio d-flex justify-content-center align-items-center p-3"
+        style={{ '--bs-aspect-ratio': '90%' } as React.CSSProperties}
+      >
+        <img alt={altText} src={imageUrl} className="bg-pokemon-card px-4 py-3" />
+      </div>
+      <div className="card-body pb-2 px-0">
+        <h3 className="card-title h5 fs-6 fw-bold">{text}</h3>
+      </div>
+    </div>
+  )
+}
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,11 @@
-import React from "react";
-import type { PokemonCardData } from "../types/Pokemon";
+import type { PokemonCardData } from '../types/Pokemon'
+import Card from './Card'
+import { capitalize } from '../utils/string'
 
 type PokemonCardProps = {
-  pokemon: PokemonCardData;
-};
+  pokemon: PokemonCardData
+}
 
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
-  return (
-    <div className="card p-2 rounded-1 bg-pokemon-card-border">
-      <div
-        className="bg-pokemon-card ratio d-flex justify-content-center align-items-center p-3"
-        style={{ "--bs-aspect-ratio": "90%" } as React.CSSProperties}
-      >
-        <img
-          alt={`Image of ${pokemon.name}`}
-          src={pokemon.imageUrl}
-          className="bg-pokemon-card px-4 py-3"
-        />
-      </div>
-      <div className="card-body pb-2 px-0">
-        <h3 className="card-title h5 fs-6 fw-bold">{pokemon.name}</h3>
-      </div>
-    </div>
-  );
+  return <Card text={capitalize(pokemon.name)} imageUrl={pokemon.imageUrl} altText={`Image of ${pokemon.name}`} />
 }
