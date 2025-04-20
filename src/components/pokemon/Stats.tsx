1e1f7fbc2f22f108b4c6961c7b78ffaea2e1d298
Lean Junio <leanjunio@outlook.com>
2025-04-20 06:00:27 -0400

    style: stats column set to 1 on mobile

--- src/components/pokemon/Stats.tsx
+++ src/components/pokemon/Stats.tsx
@@ -6,7 +6,7 @@ type StatsProps = {
 
 export default function Stats({ stats }: StatsProps) {
   return (
-    <div className="p-3 row row-cols-2 bg-stats w-100 mx-auto row-gap-2">
+    <div className="p-3 row row-cols-1 row-cols-md-2 bg-stats w-100 mx-auto row-gap-2">
       {stats.map((stat) => (
         <div className="col d-flex px-0" key={stat.name}>
           <p className="fw-bold mb-0 me-2">{stat.name}:</p>
