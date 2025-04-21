10044800b7d807167ab5e41f783213cc86817bd5
Lean Junio <leanjunio@outlook.com>
2025-04-21 11:51:46 -0400

    refactor: integrate condition in render

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -11,19 +11,17 @@ type ListProps<T extends { id: number }> = {
  * @returns {React.ReactNode} The rendered list
  */
 export default function List<T extends { id: number }>({ items, listItem }: ListProps<T>) {
-  if (items.length === 0) {
-    return (
-      <div className="min-vh-100 d-flex justify-content-center align-items-center">
-        <p className="text-center">No items found</p>
-      </div>
-    )
-  }
-
   return (
-    <ul className="min-vh-100 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 list-unstyled">
-      {items.map((item) => (
-        <li key={item.id}>{listItem(item)}</li>
-      ))}
-    </ul>
+    <div className="min-vh-100">
+      {items.length === 0 ? (
+        <p className="text-center">No items found</p>
+      ) : (
+        <ul className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 list-unstyled">
+          {items.map((item) => (
+            <li key={item.id}>{listItem(item)}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
