092c41ad397326a4d6fbb8d64373e110cfd0dccf
Lean Junio <leanjunio@outlook.com>
2025-04-21 10:01:03 -0400

    style: change min list height to full screen height

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -12,7 +12,7 @@ type ListProps<T extends { id: number }> = {
  */
 export default function List<T extends { id: number }>({ items, listItem }: ListProps<T>) {
   return (
-    <ul className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 list-unstyled">
+    <ul className="min-vh-100 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 list-unstyled">
       {items.map((item) => (
         <li key={item.id}>{listItem(item)}</li>
       ))}
