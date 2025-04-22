54e5cea1cb422c192a246d0305485c737698ef59
Lean Junio <leanjunio@outlook.com>
2025-04-22 00:31:53 -0400

    error: add restraint to retry logic on 404

--- src/hooks/useGetList.ts
+++ src/hooks/useGetList.ts
@@ -1,16 +1,21 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, UseQueryOptions } from '@tanstack/react-query'
 
 /**
  * Hook to fetch a list of items from the API
  * @param listKey - The key to store the list in the cache
  * @param queryFn - The function to fetch the list
- * @param staleTime - The time to keep the list in the cache
+ * @param options - Optional query options to override defaults
  * @returns The list of items
  */
-export function useGetList<T>(listKey: string, queryFn: () => Promise<T[]>, staleTime: number = Infinity) {
+export function useGetList<T>(
+  listKey: string,
+  queryFn: () => Promise<T[]>,
+  options?: Partial<UseQueryOptions<T[], Error>>,
+) {
   return useQuery<T[]>({
     queryKey: [`${listKey}-list`],
     queryFn,
-    staleTime,
+    staleTime: Infinity,
+    ...options,
   })
 }
--- src/hooks/usePokemonList.ts
+++ src/hooks/usePokemonList.ts
@@ -5,15 +5,17 @@ import { QUERY_CONFIG } from '@/lib/constants'
 import { NotFoundError } from '@/errors/api'
 import { UseQueryOptions } from '@tanstack/react-query'
 
+function shouldRetry(count: number, error: Error) {
+  if (error instanceof NotFoundError) {
+    return false
+  }
+  return true
+}
+
 export function usePokemonList() {
   const options: Partial<UseQueryOptions<PokemonUrls[], Error>> = {
     staleTime: QUERY_CONFIG.list.STALE_TIME,
-    retry: (_, error) => {
-      if (error instanceof NotFoundError) {
-        return false
-      }
-      return true
-    },
+    retry: shouldRetry,
   }
 
   return useGetList<PokemonUrls>('pokemon-list', pokemonApi.getPokemonList, options)
